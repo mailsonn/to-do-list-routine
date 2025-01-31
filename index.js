52c79e88012e6dcb0052edcd190fbb9018dfefae
mailsonn <mailsonpapa68@gmail.com>
2025-01-31 11:07:16 -0300

    feat: add api for advices

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 
-let add = document.querySelector('#add+');
+let add = document.querySelector('#add');
+
+let advicesArea = document.querySelector('.advices');
 
 add.addEventListener('click', function(){
     let inputTask = document.querySelector('input').value;
 
     
 })
+
+let url = 'https://api.adviceslip.com/advice';
+
+async function advices(){
+    let response = await fetch(url);
+    let data = await response.json();
+
+    console.log(data);
+    
+
+    let randomAdvice = data.slip.advice;
+    let advicesArea = document.querySelector('.advices');
+
+    advicesArea.innerHTML = randomAdvice;
+
+}
+
+advices();
+
+
